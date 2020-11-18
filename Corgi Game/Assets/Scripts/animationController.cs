0c9af09b88162cfa512169a2f1b880ba1820889d
David <deduran@nmsu.edu>
2020-11-17 21:03:22 -0700

    Addes a new method of movement to

---
+++ Corgi Game/Assets/Scripts/animationController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animationController : MonoBehaviour
+{
+    Animator animator;
+    private string currentState;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    } // end of start
+
+    // Changes the animation state based on conditions set inside
+    void ChangeAnimationState(string newState)
+    {
+        // Stop the animation from interrupting itself
+        if (currentState == newState) return;
+
+        // reassign the current state
+        currentState = newState;
+        
+    } // end of ChangeAnimationState
+} // end of class
