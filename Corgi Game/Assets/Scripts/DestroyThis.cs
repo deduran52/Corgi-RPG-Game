99f4bcc9c7feb6e5105dae216252a69cf360a89d
David <deduran@nmsu.edu>
2020-12-02 23:50:19 -0700

    Adds digging to the game

---
+++ Corgi Game/Assets/Scripts/DestroyThis.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyThis : MonoBehaviour
+{
+    public GameObject thisObject;
+    public GameObject guide;
+
+    public float range =2;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && (guide.transform.position - thisObject.transform.position).sqrMagnitude < range * range)
+        {
+            Destroy(thisObject);
+        }
+        
+    }
+}
