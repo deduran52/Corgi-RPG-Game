350eaf3e9618cbf42b86413e7202499b582f8ecd
David <deduran@nmsu.edu>
2020-12-03 21:52:32 -0700

    Adds another controller script to the files

---
+++ Corgi Game/Assets/Scripts/AnotherController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnotherController : MonoBehaviour
+{
+    public float moveSpeed = 1;
+    public Rigidbody theRB;
+    public float jumpForce = 1;
+
+    public Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Grabs the Player Rigidbody, this is attached from the Unity side
+        theRB = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        theRB.velocity = new Vector3(Input.GetAxis("Horizontal") * moveSpeed, theRB.velocity.y, Input.GetAxis("Vertical") * moveSpeed);
+
+        if(Input.GetButtonDown("Jump"))
+        {
+            theRB.velocity = new Vector3(theRB.velocity.x, jumpForce,theRB.velocity.z);
+        }
+        anim.SetFloat("speed", (Mathf.Abs(Input.GetAxis("Vertical"))+ Mathf.Abs(Input.GetAxis("Horizontal"))));
+    }
+}
